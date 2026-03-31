ae572c0c8ff4e052f03e5d74086ce957910e9351
Nikitha N <nikitha.nrb@gmail.com>
2026-03-31 23:22:12 +0530

    Create Deep Learning Algorithms.py

---
+++ Deep Learning Algorithms.py
@@ -0,0 +1,6 @@
+//Based on neural networks
+Artificial Neural Networks (ANN)
+Convolutional Neural Networks (CNN) (images)
+Recurrent Neural Networks (RNN) (sequences)
+LSTM (Long Short-Term Memory)
+GANs (Generative Adversarial Networks)
