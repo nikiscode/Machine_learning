64fb021af938d8387753fbc48f744fcf5f247b84
Nikitha N <nikitha.nrb@gmail.com>
2026-06-02 22:57:11 +0530

    Update Deep Learning Algorithms.py

--- Deep Learning Algorithms.py
+++ Deep Learning Algorithms.py
@@ -1,4 +1,4 @@
-       /ok/Based on neural network
+       //Based on neural network
 Artificial Neural Networks (ANN)
 Convolutional Neural Networks (CNN) (images)
 Recurrent Neural Networks (RNN) (sequences)
